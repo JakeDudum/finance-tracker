c26ddbbc4f66a8444195199f8de37e8281ba9946
Jake Dudum <jakedudum@gmail.com>
2022-05-03 14:22:18 -0700

    Updated stocks controller to handle improper stock symbols.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -8,12 +8,16 @@ class StocksController < ApplicationController
                     format.js { render partial: 'users/result' }
                 end
             else
-                flash[:alert] = "Please enter a valid symbol to search for"
-                redirect_to my_portfolio_path
+                respond_to do |format|
+                    flash.now[:alert] = "Please enter a valid symbol to search for"
+                    format.js { render partial: 'users/result' }
+                end
             end
         else
-            flash[:alert] = "Please enter a symbol to search for"
-            redirect_to my_portfolio_path
+            respond_to do |format|
+                flash.now[:alert] = "Please enter a symbol to search for"
+                format.js { render partial: 'users/result' }
+            end
         end
     end
 
