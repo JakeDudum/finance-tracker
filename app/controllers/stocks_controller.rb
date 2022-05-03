ba5de7fedbfe77a264fd206b354079dae048ca37
Jake Dudum <jakedudum@gmail.com>
2022-05-02 18:08:59 -0700

    Created form to search for stock symbols and display results with some basic validation.

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,20 @@
+class StocksController < ApplicationController
+
+    def search
+        if params[:stock].present?
+            @stock = Stock.new_lookup(params[:stock])
+            if @stock
+                respond_to do |format|
+                    format.js { render partial: 'users/result' }
+                end
+            else
+                flash[:alert] = "Please enter a valid symbol to search for"
+                redirect_to my_portfolio_path
+            end
+        else
+            flash[:alert] = "Please enter a symbol to search for"
+            redirect_to my_portfolio_path
+        end
+    end
+
+end
