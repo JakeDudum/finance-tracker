98411ae1b33e2f6f7be365da7ec996f626564b92
Jake Dudum <jakedudum@gmail.com>
2022-05-04 14:19:28 -0700

    Added view prolfile button to view other users profiles and the stocks that they are tracking. Refactored how stocks are displayed based on the current user.

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get 'my_friends', to: 'users#my_friends'
   get 'search_stock', to: 'stocks#search'
   get 'search_friend', to: 'users#search'
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
