e74153fafd2a274c851dd3a562e5cda006656c48
Jake Dudum <jakedudum@gmail.com>
2022-05-03 18:41:35 -0700

    Created friendships table to allow users to add other users as friends. Added my_friends view to see all friends and search for new ones. Updated controllers to handle new friends functionality with some sanitizing such as excluding self from friends search.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,30 @@ class UsersController < ApplicationController
   def my_portfolio
     @tracked_stocks = current_user.stocks
   end
+
+  def my_friends
+    @friends = current_user.friends
+  end
+
+  def search
+    if params[:friend].present?
+      @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends)
+      if @friends
+          respond_to do |format|
+              format.js { render partial: 'users/friend_result' }
+          end
+      else
+          respond_to do |format|
+              flash.now[:alert] = "Couldn't find user"
+              format.js { render partial: 'users/friend_result' }
+          end
+      end
+    else
+      respond_to do |format|
+          flash.now[:alert] = "Please enter a friend name or email to search for"
+          format.js { render partial: 'users/friend_result' }
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'my_friends', to: 'users#my_friends'
   get 'search_stock', to: 'stocks#search'
+  get 'search_friend', to: 'users#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
